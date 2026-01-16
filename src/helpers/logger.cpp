618ec4b9664adbc6a832eb9ce19d4c544ebbe933
hoppxi <hoppxii@gmail.com>
2026-01-16 18:32:54 +0300

    feat(logger): option to specify logger verbosity

--- src/helpers/logger.cpp
+++ src/helpers/logger.cpp
@@ -1,5 +1,6 @@
 #include "logger.hpp"
 
+#include <atomic>
 #include <chrono>
 #include <fstream>
 #include <iomanip>
@@ -7,6 +8,9 @@
 #include <mutex>
 
 namespace Lawnch::Logger {
+
+static constexpr int WIDTH_LEVEL = 8;
+static constexpr int WIDTH_NAME = 15;
 
 class LogEngine {
 public:
@@ -18,8 +22,10 @@ public:
   LogEngine(const LogEngine &) = delete;
   LogEngine &operator=(const LogEngine &) = delete;
 
-  void setLogFile(const std::string &path) {
+  void configure(const std::string &path, LogVerbosity verbosity) {
     std::lock_guard<std::mutex> lock(m_mutex);
+    m_verbosity = verbosity;
+
     if (m_file.is_open()) {
       m_file.close();
     }
@@ -30,29 +36,49 @@ public:
     }
   }
 
-  void write(std::string_view logger_name, Lawnch::Logger::LogLevel level,
+  void write(std::string_view logger_name, LogLevel level,
              std::string_view message) {
+    int level_val = 0;
+    switch (level) {
+    case LogLevel::CRITICAL:
+    case LogLevel::ERROR:
+      level_val = 1;
+      break;
+    case LogLevel::WARNING:
+      level_val = 2;
+      break;
+    case LogLevel::INFO:
+      level_val = 3;
+      break;
+    case LogLevel::DEBUG:
+      level_val = 4;
+      break;
+    }
+
+    if (level_val > static_cast<int>(m_verbosity.load())) {
+      return;
+    }
 
     auto now = std::chrono::system_clock::now();
     auto time_t_now = std::chrono::system_clock::to_time_t(now);
-    auto tm_now = *std::localtime(&time_t_now);
 
     std::lock_guard<std::mutex> lock(m_mutex);
+    auto tm_now = *std::localtime(&time_t_now);
 
     if (!m_file.is_open()) {
-      // fallback to console if file isn't open
       std::cout << "[LOG_FAILED] " << message << "\n";
       return;
     }
 
     m_file << "[" << std::put_time(&tm_now, "%Y-%m-%d %H:%M:%S") << "] ";
-    m_file << "[" << logger_name << "] ";
-    m_file << "[" << levelToString(level) << "] ";
+    m_file << "[" << std::right << std::setw(WIDTH_LEVEL)
+           << levelToString(level) << "] ";
+    m_file << "[" << std::right << std::setw(WIDTH_NAME) << logger_name << "] ";
     m_file << message << "\n";
   }
 
 private:
-  LogEngine() = default;
+  LogEngine() : m_verbosity(LogVerbosity::LEVEL_3) {}
 
   ~LogEngine() {
     if (m_file.is_open()) {
@@ -60,8 +86,7 @@ private:
     }
   }
 
-  std::string_view levelToString(Lawnch::Logger::LogLevel level) {
-    using Lawnch::Logger::LogLevel;
+  std::string_view levelToString(LogLevel level) {
     switch (level) {
     case LogLevel::DEBUG:
       return "DEBUG";
@@ -80,10 +105,11 @@ private:
 
   std::ofstream m_file;
   std::mutex m_mutex;
+  std::atomic<LogVerbosity> m_verbosity;
 };
 
-void init(const std::string &file_path) {
-  LogEngine::getInstance().setLogFile(file_path);
+void init(const std::string &file_path, LogVerbosity verbosity) {
+  LogEngine::getInstance().configure(file_path, verbosity);
 }
 
 void log(std::string_view logger_name, LogLevel level,
--- src/helpers/logger.hpp
+++ src/helpers/logger.hpp
@@ -5,9 +5,21 @@
 
 namespace Lawnch::Logger {
 
-enum class LogLevel { DEBUG, INFO, WARNING, ERROR, CRITICAL };
+enum class LogLevel { CRITICAL, ERROR, WARNING, INFO, DEBUG };
 
-void init(const std::string &file_path);
+// 1 = error and critical only
+// 2 = warnings + above
+// 3 = info + above
+// 4 = debug + above
+enum class LogVerbosity : int {
+  LEVEL_1 = 1,
+  LEVEL_2 = 2,
+  LEVEL_3 = 3,
+  LEVEL_4 = 4
+};
+
+void init(const std::string &file_path,
+          LogVerbosity verbosity = LogVerbosity::LEVEL_3);
 void log(std::string_view logger_name, LogLevel level,
          std::string_view message);
 

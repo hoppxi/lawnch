dc8798208e0ed62ea0e1077a7fba996eca6bcc90
hoppxi <hoppxii@gmail.com>
2026-01-23 22:06:23 +0300

    fix: symlink paths to profile and use the new plugin structure

--- nix/default.nix
+++ nix/default.nix
@@ -14,9 +14,6 @@ let
   cfg = config.programs.lawnch;
   toIni = pkgs.formats.ini { };
 
-  configDir = "lawnch";
-  fullConfigPath = "${config.xdg.configHome}/${configDir}";
-
   generateFullConfig =
     let
       enabledPlugins = filterAttrs (_: v: v.enable) cfg.plugins;
@@ -55,77 +52,18 @@ let
 
   themeIniFiles = collectIniFiles "${lawnch-unwrapped}/lib/lawnch/themes";
 
-  themeLinks = builtins.listToAttrs (
-    map (
-      file:
-      let
-        base = builtins.baseNameOf file;
-      in
-      {
-        name = "lawnch/themes/${builtins.unsafeDiscardStringContext base}";
-        value = {
-          source = file;
-        };
-      }
-    ) themeIniFiles
-  );
-
 in
 {
   imports = [ (import ./lawnch.nix { inherit lib lawnch; }) ];
 
   config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
 
     xdg.configFile = mkMerge [
-      (mkIf (!cfg.mutable) (mkMerge [
-        { "lawnch/config.ini".source = generateFullConfig; }
-        (mapAttrs' (name: source: nameValuePair "lawnch/${name}.ini" { inherit source; }) menuSources)
-      ]))
+      { "lawnch/config.ini".source = generateFullConfig; }
+      (mapAttrs' (name: source: nameValuePair "lawnch/${name}.ini" { inherit source; }) menuSources)
     ];
 
-    home.activation.backupLawnchConfigs = mkIf (!cfg.mutable) (
-      lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-        backup_if_real() {
-          local f="$1"
-          if [ -e "$f" ] && [ ! -L "$f" ]; then
-            local i=1
-            while [ -e "$f.b$i" ]; do i=$((i+1)); done
-            $DRY_RUN_CMD mv "$f" "$f.b$i"
-          fi
-        }
-        backup_if_real "${fullConfigPath}/config.ini"
-        ${concatStringsSep "\n" (
-          map (name: ''
-            backup_if_real "${fullConfigPath}/${name}.ini"
-          '') (builtins.attrNames cfg.menus)
-        )}
-      ''
-    );
-
-    home.activation.setupLawnchConfig = mkIf cfg.mutable (
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        $DRY_RUN_CMD mkdir -p "${fullConfigPath}"
-
-        setup_mutable() {
-          local src="$1"
-          local dest="$2"
-          if [ ! -f "$dest" ]; then
-            $DRY_RUN_CMD cp -f "$src" "$dest"
-            $DRY_RUN_CMD chmod 600 "$dest"
-          fi
-        }
-
-        setup_mutable "${generateFullConfig}" "${fullConfigPath}/config.ini"
-        ${concatStringsSep "\n" (
-          mapAttrsToList (name: source: ''
-            setup_mutable "${source}" "${fullConfigPath}/${name}.ini"
-          '') menuSources
-        )}
-      ''
-    );
-
-    xdg.dataFile =
+    home.packages =
       let
         buildPlugin =
           name: plugin:
@@ -137,45 +75,37 @@ in
           };
 
         enabledPlugins = filterAttrs (_: v: v.enable) cfg.plugins;
-        enabledPluginPkgs = mapAttrsToList (name: plugin: buildPlugin name plugin) enabledPlugins;
 
-        mergedAssets = pkgs.runCommand "lawnch-merged-assets" { } ''
-          mkdir -p $out
-          ${concatStringsSep "\n" (
-            map (pkg: ''
-              if [ -d "${pkg}/lib/lawnch/assets" ]; then
-                cp -rsnf ${pkg}/lib/lawnch/assets/. $out/
-                chmod -R +w $out/ || true
-              fi
-            '') enabledPluginPkgs
-          )}
-        '';
-      in
-      mkMerge (
-        [
-          themeLinks
-          {
-            "lawnch/assets" = {
-              source = mergedAssets;
-              recursive = true;
-            };
-          }
-        ]
-        ++ (mapAttrsToList (
+        wrapPluginForProfile =
           name: plugin:
           let
             builtPkg = buildPlugin name plugin;
           in
-          {
-            "lawnch/plugins/${name}.so" = {
-              source = "${builtPkg}/lib/lawnch/plugins/${name}.so";
-              executable = true;
-            };
-            "lawnch/plugins/pinfo/${name}" = {
-              source = "${builtPkg}/lib/lawnch/plugins/pinfo/${name}";
-            };
-          }
-        ) enabledPlugins)
-      );
+          pkgs.runCommand "lawnch-plugin-${name}-profile" { } ''
+            mkdir -p $out/share/lawnch/plugins/${name}
+            cp ${builtPkg}/lib/lawnch/plugins/${name}.so $out/share/lawnch/plugins/${name}/
+
+            if [ -f ${builtPkg}/lib/lawnch/plugins/pinfo/${name} ]; then
+              cp ${builtPkg}/lib/lawnch/plugins/pinfo/${name} $out/share/lawnch/plugins/${name}/pinfo
+            fi
+
+            if [ -d ${builtPkg}/lib/lawnch/plugins/assets ]; then
+              mkdir -p $out/share/lawnch/plugins/${name}/assets
+              cp -r ${builtPkg}/lib/lawnch/plugins/assets/* $out/share/lawnch/plugins/${name}/assets/
+            fi
+          '';
+
+        pluginPackages = mapAttrsToList wrapPluginForProfile enabledPlugins;
+
+        themesPackage = pkgs.runCommand "lawnch-themes-profile" { } ''
+          mkdir -p $out/share/lawnch/themes
+          ${concatStringsSep "\n" (map (file: "cp ${file} $out/share/lawnch/themes/") themeIniFiles)}
+        '';
+      in
+      [
+        cfg.package
+        themesPackage
+      ]
+      ++ pluginPackages;
   };
 }

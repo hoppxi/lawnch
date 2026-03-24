25e137507331783f740afde97a30d3254bbb2a58
hoppxi <152753390+hoppxi@users.noreply.github.com>
2026-03-24 11:39:37 +0300

    refactor(nix): refactor code and fix nixosModule not working

--- nix/default.nix
+++ nix/default.nix
@@ -1,80 +1,66 @@
 {
-  pkgs,
-  lawnch,
-  available-plugins ? { },
-  platform ? "home-manager",
+  self,
+  nixpkgs,
+  lawnch-plugins,
   ...
 }:
-{
-  config,
-  lib,
-  ...
-}:
-
-with lib;
 
 let
-  cfg = config.programs.lawnch;
-  toTOML = pkgs.formats.toml { };
-  isHm = platform == "home-manager";
-
-  generateFullConfig =
-    let
-      enabledPlugins = filterAttrs (_: v: v.enable) cfg.plugins;
-
-      pluginSections = {
-        plugin = mapAttrs (
-          name: pcfg: { enable = true; } // pcfg.settings
-        ) enabledPlugins;
-      };
-
-      finalSettings = cfg.settings // pluginSections;
-    in
-    toTOML.generate "config.toml" finalSettings;
-
-  menuSources = mapAttrs (name: val: toTOML.generate "${name}.toml" val) cfg.menus;
-
-  pluginPackages =
-    let
-      enabledPlugins = filterAttrs (_: v: v.enable) cfg.plugins;
-    in
-    mapAttrsToList (
-      name: pcfg:
-      if pcfg.package != null then
-        pcfg.package
-      else if hasAttr name available-plugins then
-        available-plugins.${name}
-      else
-        throw "Plugin '${name}' not found in official registry."
-    ) enabledPlugins;
-
-  allPackages = [
-    cfg.package
-    lawnch
-  ]
-  ++ pluginPackages;
-
-  lawnchConfig = mkMerge [
-    { "lawnch/config.toml".source = generateFullConfig; }
-    (mapAttrs' (name: src: nameValuePair "lawnch/${name}.toml" { source = src; }) menuSources)
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
   ];
+
+  forAllSystems = nixpkgs.lib.genAttrs systems;
+
+  mkModule =
+    platform:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      system = pkgs.stdenv.hostPlatform.system;
+    in
+    import ./module.nix {
+      inherit pkgs platform;
+      lawnch = self.packages.${system}.default;
+      available-plugins = lawnch-plugins.packages.${system};
+    } { inherit config lib; };
 
 in
 {
-  imports = [
-    (import ./options.nix { inherit lib lawnch; })
-  ];
-
-  config = mkIf cfg.enable (
-    if isHm then
-      {
-        xdg.configFile = lawnchConfig;
-        home.packages = allPackages;
-      }
-    else
-      {
-        environment.etc = mapAttrs' (name: value: nameValuePair "xdg/${name}" value) lawnchConfig;
-        environment.systemPackages = allPackages;
-      }
+  packages = forAllSystems (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      lawnch-plugin-api = lawnch-plugins.packages.${system}.plugin-api;
+    in
+    {
+      default = import ./package.nix { inherit pkgs lawnch-plugin-api; };
+    }
   );
+
+  devShells = forAllSystems (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      lawnch = self.packages.${system}.default;
+    in
+    {
+      default = import ./devshell.nix { inherit pkgs lawnch; };
+    }
+  );
+
+  homeModules = {
+    default = mkModule "home-manager";
+    lawnch = mkModule "home-manager";
+  };
+
+  nixosModules = {
+    default = mkModule "nixos";
+    lawnch = mkModule "nixos";
+  };
 }
